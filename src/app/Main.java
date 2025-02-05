9bfe217d984154219627971633f6d83d9ca891d8
undefinedDen <tunevdenis123@gmail.com>
2025-02-05 12:31:09 +0200

    add convertation from Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,27 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static double fahrenheits;
+static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Convert app");
+        System.out.println("""
+                Convert app!
+                Version 1.0;
+                I can convert only from Fahrenheit to Celsius!""");
+        fahrenheits = getInput("Fahrenheit: ");
+getOutput("Fahrenheit", "Celsius", convertFromFahrenheitToCelsius());
+    }
+
+    private static double convertFromFahrenheitToCelsius() {
+        return ((fahrenheits - 32) * 5) / 9 ;
+    }
+    private static double getInput(String input){
+        System.out.print("Input " + input);
+        return scanner.nextDouble();
+    }
+    private static void getOutput(String convertFrom, String convertTo, double result){
+        System.out.printf("Result of converting from %s to %s is %.2f %s",convertFrom, convertTo, result, convertTo);
     }
 }
